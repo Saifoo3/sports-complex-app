87f2cf3fa704def15964bd233cc3d66cb8fb4288
Saifoo <saif.azeem333@gmail.com>
2023-12-14 11:15:49 +0400

    im done at this point

--- App.js
+++ App.js
@@ -1,13 +1,24 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 import Login from './App/Pages/Login';
+import Home from './App/Pages/Home';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
 
 
+const Stack = createStackNavigator();
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Login/>
-    </View>
+    // <View style={styles.container}>
+    //   <Login/>
+    // </View>
+
+    <NavigationContainer>
+    <Stack.Navigator>
+      <Stack.Screen name="Login" component={Login} />
+      <Stack.Screen name="Home" component={Home} />
+    </Stack.Navigator>
+  </NavigationContainer>
   );
 }
 
--- App/Pages/Login.js
+++ App/Pages/Login.js
@@ -1,31 +1,103 @@
-import { View, Text, Image, StyleSheet } from 'react-native'
-import React from 'react'
+import { View, Text, Image, StyleSheet, TextInput, Button } from 'react-native'
+import React, { useEffect, useState } from 'react'
 import colours from '../Tempcolours/colours';
 import { Ionicons } from '@expo/vector-icons';
 import * as WebBrowser from 'expo-web-browser';
 import * as Google from 'expo-auth-session/providers/google';
+import { TouchableOpacity } from 'react-native';
 
-export default function Login() {
+export default function Login({ navigation }) {
   WebBrowser.maybeCompleteAuthSession();
+  const [accessToken, setAccessToken]=useState();
   const [request, response, promptAsync] = Google.useAuthRequest({
     androidClientId: '722134148334-pvke7chr3p0hlutmdo02jrqq1l6tu9bh.apps.googleusercontent.com',
     expoClientId:'722134148334-2e17cpd2a5i0kbs0u9vnro5g7mdsk4cq.apps.googleusercontent.com'
 
   });
+
+
+  useEffect(()=>{
+      if(response?.type=='cancel')
+      {
+          setAccessToken(response.authentication.accessToken);
+          console.log(response.authentication.accessToken);
+          getUserData();
+      }
+  }, [response]);
+
+      const getUserData=async()=>{
+        try {
+          const resp = await fetch(
+            "https://www.googleapis.com/userinfo/v2/me",
+            {
+              headers: { Authorization: `Bearer ${response.authentication.accessToken}` },
+            }
+          );
+    
+          const user = await resp.json();
+          console.log("user Details",user) 
+          setUserInfo(user); 
+          setUserData(user);
+          await Services.setUserAuth(user);
+        } catch (error) {
+          // Add your own error handler here
+        }
+      }
+
+      const [email, setEmail] = useState('');
+      const [password, setPassword] = useState('');
+    
+      const handleEmailChange = (text) => {
+        setEmail(text);
+      };
+    
+      const handlePasswordChange = (text) => {
+        setPassword(text);
+      };
+    
+      const handleSignupWithEmail = () => {
+        // Implement your signup with email and password logic here
+        console.log('Email:', email);
+        console.log('Password:', password);
+        // Add your authentication logic, API calls, etc.
+
+        navigation.navigate('Home');
+      };
   return (
     <View>
         <Image source={require('./../Assets/Images/login.png')}/>
         <View style={styles.container}>
             <Text style={styles.welcomeText}>Welcome to the Sports Complex</Text>
-            <Text style={{textAlign:'center',marginTop:70,fontSize:23}}>Login/Signup</Text>
-            <View style={styles.button}>
+            {/* <Text style={{textAlign:'center',marginTop:70,fontSize:23}}>Login/Signup</Text> */}
+            <TouchableOpacity style={styles.button} 
+            onPress={()=>promptAsync()}>
               <Ionicons name="logo-google" size={24} 
               color="white" style={{marginRight:10}}/>
               <Text style={{color:colours.white ,fontSize:15}}>Sign in using Google</Text>
-            </View>
-        </View>
+            </TouchableOpacity>
+            <Text style={{textAlign:'center',marginTop:1,fontSize:23}}>-or-</Text>
+            
+                    {/* Email and Password Signup */}
+        <TextInput
+          style={styles.input}
+          placeholder="Email"
+          onChangeText={handleEmailChange}
+          keyboardType="email-address"
+          autoCapitalize="none"
+        />
+        <TextInput
+          style={styles.input}
+          placeholder="Password"
+          onChangeText={handlePasswordChange}
+          secureTextEntry
+        />
+      <TouchableOpacity style={styles.button} onPress={handleSignupWithEmail}>
+        <Text style={{ color: colours.white, fontSize: 15 }}>Signup with Email</Text>
+      </TouchableOpacity>
+      </View>
     </View>
-  )
+    
+  );
 }
 
 const styles = StyleSheet.create({
@@ -39,16 +111,27 @@ const styles = StyleSheet.create({
     welcomeText:{
       fontSize:35,
       textAlign:'center',
-      fontWeight:'bold'
+      fontWeight:'bold',
+      marginBottom:1
     },
     button:{
       backgroundColor:colours.primary,
       padding:10,
-      margin:50,
+      margin:15,
       display:'flex',
       flexDirection:'row',
       justifyContent:'center',
       alignItems:'center',
       borderRadius:10
-    }
+    },
+    input: {
+      width: '100%',
+      height: 40,
+      borderColor: 'gray',
+      borderWidth: 1,
+      justifyContent:'center',
+      alignItems:'center',
+      marginBottom: 20,
+      padding: 10,
+    },
 });
